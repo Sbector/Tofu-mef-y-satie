cdf527c97003dc867302f2f51e7be75346191e5e
No_otra_vez <saulbector@gmail.com>
2017-12-06 16:18:41 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
+$(function() {
+  $('#video').css({
+    width: $(window).innerWidth() + 'px',
+    height: $(window).innerHeight() + 'px'
+  });
 
-$(function(){
-$('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
-
-$(window).resize(function(){
-$('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
-	
+  $(window).resize(function() {
+    $('#video').css({
+      width: $(window).innerWidth() + 'px',
+      height: $(window).innerHeight() + 'px'
+    });
+  });
+});
